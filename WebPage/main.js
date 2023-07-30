9e36b410b04a0e04f44dfe8631546e35cb3fdc7f
Spencer T. Parkin <spencertparkin@gmail.com>
2023-07-29 23:06:51 -0600

    Add message if WebGL not available, as per the recommendation in the tutorial.

--- WebPage/main.js
+++ WebPage/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -23,4 +24,12 @@ function animate() {
 	renderer.render(scene, camera);
 }
 
-animate();
+if (WebGL.isWebGLAvailable()) {
+
+	// Initiate function or other initializations here
+	animate();
+
+} else {
+	const warning = WebGL.getWebGLErrorMessage();
+	document.getElementById('container').appendChild(warning);
+}
