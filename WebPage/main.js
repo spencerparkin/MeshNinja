59edd6ca8349ff4d07476dcbcf73d016c3c2b58a
Spencer T. Parkin <spencertparkin@gmail.com>
2023-07-29 23:21:11 -0600

    Have the page now load my own custom-built GLTF file.  It loads, but doesn't render, maybe because there are no lights in the scene?

--- WebPage/main.js
+++ WebPage/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -8,19 +9,32 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+//const geometry = new THREE.BoxGeometry(1, 1, 1);
+//const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+//const cube = new THREE.Mesh(geometry, material);
+//scene.add(cube);
 
-camera.position.z = 5;
+const loader = new GLTFLoader();
+
+loader.load('public/test.gltf', function(gltf) {
+	console.log('Loaded!');
+	scene.add(gltf.scene);
+}, undefined, function(error) {
+	console.error(error);
+});
+
+camera.position.set(0, 0, 10);
+camera.lookAt(0, 0, 0);
 
 function animate() {
 	requestAnimationFrame(animate);
 	
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	//cube.rotation.x += 0.01;
+	//cube.rotation.y += 0.01;
 	
+	// TODO: Maybe this doesn't work, because there are no lights in the scene?
+	//       Note that my GLTF file does load here: https://gltf-viewer.donmccurdy.com/
+	//       So that is promising.
 	renderer.render(scene, camera);
 }
 
