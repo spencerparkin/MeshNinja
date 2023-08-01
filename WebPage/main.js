6099f3abfa1ec2505ca88d47e3c2b289ff038396
Spencer T. Parkin <Spencer.Parkin@L3T.com>
2023-08-01 12:52:53 -0600

    Swap out test GLTF file for a maze.

--- WebPage/main.js
+++ WebPage/main.js
@@ -4,24 +4,22 @@ import { GLTFLoader } from "/node_modules/.vite/deps/three_addons_loaders_GLTFLo
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setClearColor(0x000000);
 renderer.setSize(window.innerWidth, window.innerHeight);
+
 document.body.appendChild(renderer.domElement);
 
 const loader = new GLTFLoader();
-let mesh = undefined;
 
-loader.load('test.gltf', function(gltf) {
-	console.log('Loaded!');
-	scene.add(gltf.scene);
-	mesh = gltf.scene.children[0];
+loader.load('Maze.gltf', function(data) {
+	scene.add(data.scene);
 }, undefined, function(error) {
 	console.error(error);
 });
 
-camera.position.set(0, 0, 20);
+camera.position.set(0, 0, 50);
 camera.lookAt(0, 0, 0);
 
 //const light = new THREE.AmbientLight(0xFFFFFF, 1.0);
@@ -31,21 +29,15 @@ const directionalLight = new THREE.DirectionalLight( 0xffffff, 1.0 );
 directionalLight.position.set(1.0, 1.0, 1.0);
 scene.add( directionalLight );
 
-function animate() {
-	requestAnimationFrame(animate);
+function run() {
+	requestAnimationFrame(run);
 	
-    if(mesh !== undefined) {
-		mesh.rotation.x += 0.01;
-		mesh.rotation.y += 0.02;
-	}
-    
 	renderer.render(scene, camera);
 }
 
 if (WebGL.isWebGLAvailable()) {
 
-	// Initiate function or other initializations here
-	animate();
+	run();
 
 } else {
 	const warning = WebGL.getWebGLErrorMessage();
